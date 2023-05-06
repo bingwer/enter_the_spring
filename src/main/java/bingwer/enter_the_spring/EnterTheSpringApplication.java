29b69714b58eea5b717004bc210ebbbb5405b239
Nick <hyolee@alphacode.co.kr>
2023-05-06 16:09:31 +0900

    initial commit

---
+++ src/main/java/bingwer/enter_the_spring/EnterTheSpringApplication.java
@@ -0,0 +1,13 @@
+package bingwer.enter_the_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterTheSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterTheSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bingwer/enter_the_spring/EnterTheSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package bingwer.enter_the_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterTheSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
